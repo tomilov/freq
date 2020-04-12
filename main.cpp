3af0d45a4b1de4da3b1aefa48d184b63cd9e77dd
Anatoliy Tomilov <tomilovanatoliy@gmail.com>
2020-04-12 20:11:22 +0500

    1.749s

--- main.cpp
+++ main.cpp
@@ -47,6 +47,7 @@ int main(int argc, char * argv[])
     InputStream<> inputStream{inputFile.get()}; // InputStream::buffer lies on the stack
 
     std::vector<TrieNode> trie(1);
+    trie.reserve(501266);
     size_t index = 0;
     for (;;) {
         int c = inputStream.getChar();
@@ -72,67 +73,36 @@ int main(int argc, char * argv[])
 
     timer.report("build counting trie from input");
 
-    std::vector<uchar> word, words;
     std::vector<std::pair<size_t, size_t>> rank;
+    rank.reserve(213637);
+    std::vector<uchar> words;
+    words.reserve(1883055);
 
-    if ((true)) {
-        auto traverseTrie = [&] (const auto & traverseTrie, decltype((std::as_const(trie).front().children)) children) -> void
-        {
-            for (size_t index : children) {
-                if (index == 0) {
-                    continue;
-                }
-                const TrieNode & node = trie[index];
-                word.push_back(uchar(node.c));
-                if (node.count != 0) {
-                    rank.emplace_back(node.count, words.size());
-                    words.insert(words.cend(), std::cbegin(word), std::cend(word));
-                    words.push_back(uchar('\0'));
-                }
-                traverseTrie(traverseTrie, node.children);
-                word.pop_back();
+    std::vector<uchar> word;
+    auto traverseTrie = [&] (const auto & traverseTrie, decltype((std::as_const(trie).front().children)) children) -> void
+    {
+        for (size_t index : children) {
+            if (index == 0) {
+                continue;
             }
-        };
-        traverseTrie(traverseTrie, trie.front().children);
-        assert(word.empty());
-        fprintf(stderr, "word count = %zu\n", rank.size());
-
-        timer.report("recover words from trie"); // 58 10
-
-        std::stable_sort(std::begin(rank), std::end(rank), [] (auto && l, auto && r) { return r.first < l.first; });
-    } else {
-        assert(index == 0);
-        for (const TrieNode & leaf : trie) {
-            if (leaf.count != 0) {
-                word.clear();
-                size_t parent = index;
-                do {
-                    const TrieNode & node = trie[parent];
-                    word.push_back(uchar(node.c));
-                    parent = node.parent;
-                } while (parent != 0);
-                rank.emplace_back(leaf.count, words.size());
-                std::reverse(std::begin(word), std::end(word));
+            const TrieNode & node = trie[index];
+            word.push_back(uchar(node.c));
+            if (node.count != 0) {
+                rank.emplace_back(node.count, words.size());
                 words.insert(words.cend(), std::cbegin(word), std::cend(word));
                 words.push_back(uchar('\0'));
             }
-            ++index;
+            traverseTrie(traverseTrie, node.children);
+            word.pop_back();
         }
+    };
+    traverseTrie(traverseTrie, trie.front().children);
+    assert(word.empty());
+    fprintf(stderr, "word count = %zu, length = %zu\n", rank.size(), words.size());
 
-        timer.report("recover words from trie"); // 37 60
+    timer.report("recover words from trie");
 
-        auto less = [words = words.data(), wordsEnd = &words.back() + 1] (auto && l, auto && r)
-        {
-            if (r.first < l.first) {
-                return true;
-            } else if (l.first < r.first) {
-                return false;
-            } else {
-                return std::lexicographical_compare(words + l.second, wordsEnd, words + r.second, wordsEnd);
-            }
-        };
-        std::stable_sort(std::begin(rank), std::end(rank), less);
-    }
+    std::stable_sort(std::begin(rank), std::end(rank), [] (auto && l, auto && r) { return r.first < l.first; });
 
     timer.report("rank words");
 
