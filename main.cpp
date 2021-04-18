b2fc5ec85273dff6d0fb72fb942be238629eca03
Anatoliy Tomilov <tomilovanatoliy@gmail.com>
2021-04-18 21:24:21 +0500

    #

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ constexpr uint16_t kDefaultChecksumHigh = ~uint16_t(0);
 struct alignas(kHardwareConstructiveInterferenceSize) Chunk
 {
     __m128i checksumHigh = _mm_set1_epi16(kDefaultChecksumHigh);
-    uint32_t count[sizeof(__m128i) / sizeof(uint16_t)] = {};
+    int count[sizeof(__m128i) / sizeof(uint16_t)] = {};
 };
 #pragma pack(pop)
 static_assert(sizeof(Chunk) == kHardwareConstructiveInterferenceSize);
@@ -48,7 +48,7 @@ constexpr auto kHashTableOrder = std::numeric_limits<uint16_t>::digits + 1; // o
 Chunk hashTable[1u << kHashTableOrder];
 alignas(__m128i) char output[std::extent_v<decltype(input)> + 2]; // provide space for leading unused char and trailing null
 auto o = output + 1; // words[i][j] == std::distance(output, o) is 0 only for unused hashes
-alignas(kHardwareConstructiveInterferenceSize) uint32_t words[std::extent_v<decltype(hashTable)>][std::extent_v<decltype(Chunk::count)>] = {};
+alignas(kHardwareConstructiveInterferenceSize) int words[std::extent_v<decltype(hashTable)>][std::extent_v<decltype(Chunk::count)>] = {};
 
 #ifdef _MSC_VER
 void incCounter(uint32_t checksum, const char * wordEnd, uint8_t len)
@@ -60,8 +60,8 @@ void incCounter(uint32_t checksum, const char * __restrict__ wordEnd, uint8_t le
     Chunk & chunk = hashTable[checksumLow];
     uint16_t checksumHigh = checksum >> kHashTableOrder;
     __m128i checksumsHigh = _mm_load_si128(&chunk.checksumHigh);
-    uint16_t m = uint16_t(_mm_movemask_epi8(_mm_cmpeq_epi16(checksumsHigh, _mm_set1_epi16(checksumHigh))));
-
+    __m128i mask = _mm_cmpeq_epi16(checksumsHigh, _mm_set1_epi16(checksumHigh));
+    uint16_t m = uint16_t(_mm_movemask_epi8(mask));
 #if defined(_MSC_VER) || defined(__MINGW32__)
     unsigned long index;
 #elif defined(__clang__) || defined(__GNUG__)
@@ -69,19 +69,8 @@ void incCounter(uint32_t checksum, const char * __restrict__ wordEnd, uint8_t le
 #else
 # error "!"
 #endif
-    if (m != 0) {
-        //assert(_mm_popcnt_u32(m) == 2);
-#if defined(_MSC_VER) || defined(__MINGW32__)
-        _BitScanForward(&index, m);
-#elif defined(__clang__) || defined(__GNUG__)
-        index = __bsfd(m);
-#else
-# error "!"
-#endif
-        index /= 2;
-    } else {
-        m = uint16_t(_mm_movemask_epi8(checksumsHigh)) & 0b1010101010101010u;
-        assert(m != 0); // fire if there is more then 8 collisions by checksumLow
+    if (m == 0) {
+        m = uint16_t(_mm_movemask_epi8(checksumsHigh)) & 0b1010101010101010;
 #if defined(_MSC_VER) || defined(__MINGW32__)
         _BitScanForward(&index, m);
 #elif defined(__clang__) || defined(__GNUG__)
@@ -94,8 +83,16 @@ void incCounter(uint32_t checksum, const char * __restrict__ wordEnd, uint8_t le
         words[checksumLow][index] = uint32_t(std::distance(output, o));
         o = std::copy_n(std::prev(wordEnd, len), len, o);
         *o++ = '\0';
+    } else {
+#if defined(_MSC_VER) || defined(__MINGW32__)
+        _BitScanForward(&index, m);
+#elif defined(__clang__) || defined(__GNUG__)
+        index = __bsfd(m);
+#else
+# error "!"
+#endif
+        index /= 2;
     }
-    assert(index < std::extent_v<decltype(chunk.count)>);
     ++chunk.count[index];
 }
 
@@ -108,17 +105,15 @@ void countWords()
         __m128i lowercase = _mm_add_epi8(_mm_and_si128(_mm_cmplt_epi8(str, _mm_set1_epi8('a')), _mm_set1_epi8('a' - 'A')), str);
         __m128i mask = _mm_or_si128(_mm_cmplt_epi8(lowercase, _mm_set1_epi8('a')), _mm_cmpgt_epi8(lowercase, _mm_set1_epi8('z')));
         uint16_t m = uint16_t(_mm_movemask_epi8(mask));
-#define BYTE(offset)                                                                \
-        if ((m & (1u << offset)) == 0) {                                            \
-            assert(len != std::numeric_limits<decltype(len)>::max());               \
-            ++len;                                                                  \
-            checksum = _mm_crc32_u8(checksum, _mm_extract_epi8(lowercase, offset)); \
-        } else {                                                                    \
-            if (len > 0) {                                                          \
-                incCounter(checksum, in + offset, len);                             \
-                len = 0;                                                            \
-                checksum = kInitialChecksum;                                        \
-            }                                                                       \
+#define BYTE(offset)                                                                    \
+        if ((m & (1u << offset)) == 0) {                                                \
+            assert(len != std::numeric_limits<decltype(len)>::max());                   \
+            ++len;                                                                      \
+            checksum = _mm_crc32_u8(checksum, _mm_extract_epi8(lowercase, offset));     \
+        } else if (len != 0) {                                                          \
+            incCounter(checksum, in + offset, len);                                     \
+            len = 0;                                                                    \
+            checksum = kInitialChecksum;                                                \
         }
         BYTE(0);
         BYTE(1);
@@ -144,6 +139,91 @@ void countWords()
 }
 
 } // namespace
+
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+
+static void findPerfectHash()
+{
+    Timer timer;
+
+    for (auto in = input; in < i; in += sizeof(__m128i)) {
+        __m128i str = _mm_load_si128(reinterpret_cast<const __m128i *>(in));
+        __m128i lowercase = _mm_add_epi8(str, _mm_and_si128(_mm_cmplt_epi8(str, _mm_set1_epi8('a')), _mm_set1_epi8('a' - 'A')));
+        __m128i mask = _mm_or_si128(_mm_cmplt_epi8(lowercase, _mm_set1_epi8('a')), _mm_cmpgt_epi8(lowercase, _mm_set1_epi8('z')));
+        _mm_store_si128(reinterpret_cast<__m128i *>(in), _mm_andnot_si128(mask, lowercase));
+    }
+    timer.report("tolower input");
+
+    const auto getWords = []() -> std::vector<std::string_view>
+    {
+        std::set<std::string_view> words;
+        auto lo = std::find(input, i, '\0');
+        while (lo != i) {
+            auto hi = std::find_if(lo, i, [](char c) { return c != '\0'; });
+            lo = std::find(hi, i, '\0');
+            words.emplace(hi, lo);
+        }
+        return {std::begin(words), std::end(words)};
+    };
+    auto words = getWords();
+    timer.report("collect words");
+    fprintf(stderr, "%zu words read\n", words.size());
+
+    std::stable_sort(std::begin(words), std::end(words), [](auto && lhs, auto && rhs) { return lhs.size() < rhs.size(); });
+    timer.report("sort words by length");
+
+    constexpr auto hashTableOrder = 17;
+    constexpr auto maxCollisions = 8;
+    const auto statusPeriod = 1000 / omp_get_num_threads();
+    int iterationCount = 0;
+    size_t maxPrefix = 0;
+    std::unordered_set<uint32_t> hashesFull;
+    std::vector<uint8_t> hashesLow;
+#pragma omp parallel for firstprivate(timer, iterationCount, maxPrefix, hashesFull, hashesLow) schedule(static, 1)
+    for (uint32_t initialChecksum = 0; initialChecksum <= std::numeric_limits<uint32_t>::max(); ++initialChecksum) {
+        bool bad = false;
+        for (const auto & word : words) {
+            uint32_t hash = initialChecksum;
+            for (char c : word) {
+                hash = _mm_crc32_u8(hash, c);
+            }
+            if (!hashesFull.insert(hash).second) {
+                bad = true;
+                break;
+            }
+        }
+        if (bad) {
+            hashesFull.clear();
+        } else {
+            bool bad = false;
+            uint8_t collision = 0;
+            hashesLow.resize(size_t(1) << hashTableOrder);
+            size_t prefix = 0;
+            for (uint32_t hash : hashesFull) {
+                ++prefix;
+                collision = std::max(collision, ++hashesLow[hash & ((1u << hashTableOrder) - 1u)]);
+                if (collision > maxCollisions) {
+                    bad = true;
+                    break;
+                }
+            }
+            hashesFull.clear();
+            if (prefix > maxPrefix) {
+                maxPrefix = prefix;
+            }
+            hashesLow.clear();
+            if (!bad) {
+                fprintf(stderr, "FOUND: iterationCount %i ; initialChecksum = %u ; collision = %hhu ; hashTableOrder %u ; time %.3lf ; tid %i\n", iterationCount, initialChecksum, collision, hashTableOrder, timer.dt(), omp_get_thread_num());
+            }
+        }
+        if ((++iterationCount % statusPeriod) == 0) {
+            fprintf(stderr, "failed at %zu of %zu\n", std::exchange(maxPrefix, 0), words.size());
+            fprintf(stderr, "status: totalIterationCount %i ; tid %i ; initialChecksum %u ; time %.3lf\n", iterationCount * omp_get_num_threads(), omp_get_thread_num(), initialChecksum, timer.dt());
+        }
+    }
+}
 
 int main(int argc, char * argv[])
 {
@@ -179,6 +259,11 @@ int main(int argc, char * argv[])
         std::fill(input + size, i, '\0');
     }
     timer.report("read input");
+
+    if ((false)) {
+        findPerfectHash();
+        return 0;
+    }
 
     countWords();
     timer.report("count words");
